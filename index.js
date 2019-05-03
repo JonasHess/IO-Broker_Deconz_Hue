967dddc6338df0576ab219ebae4f1bb8b9cc74d3
JonasHess <JonasHess@users.noreply.github.com>
2019-05-04 01:29:37 +0200

    Updated Index.js

--- index.js
+++ index.js
@@ -1,135 +1,100 @@
-var DimmerStatePath = "Sensors.HueDimmer";
 
-function currentDate() {
-    var d = new Date();
-    return new Date(d.getFullYear(), d.getMonth(), d.getDate());
-}
-
-function getButtonFromButtonId(buttonId) {
-    return Math.floor((buttonId / 1000)) * 1000;
-}
-
-function registerHueDimmerButtonEvents(dimmerName, dimmerObjectID, funcObjc) {
+function registerHueDimmerButtonEvents(dimmerName, dimmerObjectID, buttonActions) {
     on({id: dimmerObjectID, change: "ne"}, function (obj) {
-        var hueDimmerButtonId = obj.state.val;
-        var oldValue = obj.oldState.val;
 
-        var statePathLastButton = DimmerStatePath + "." + dimmerName +"."+ "LastButton";
-        var statePathClickHistory = DimmerStatePath + "." + dimmerName +"."+ hueDimmerButtonId;
-
-        var registerEvents = function ()  {
-
-            var buttonActionsMap = funcObjc[hueDimmerButtonId.toString()];
-            if (!buttonActionsMap) {
-                return;
-            }
-            if (buttonActionsMap.length == 0) {
-                return
-            }
-
-            var maxArrayLength = buttonActionsMap.length;
-
-
-            // Get Last Button State
-            var currentButton = getButtonFromButtonId(hueDimmerButtonId);
-            var lastButton = getState(statePathLastButton).val;
-            
-            // Update State LastButton
-            setState(statePathLastButton, currentButton);
-
-
-            var stateValue = JSON.parse(getState(statePathClickHistory).val);
-            if (! (lastButton == currentButton)) {
-                stateValue.History = [];
-            }else if (stateValue.History.length >= 1) {
-                var milisecLastButtonPress =  stateValue.History[Math.max(stateValue.History.length - 1)];
-                var milisecCurrentButtonPres = Date.now();
-                console.log((milisecCurrentButtonPres - milisecLastButtonPress));
-                if ((milisecCurrentButtonPres - milisecLastButtonPress) > 3000)  {
-                    stateValue.History = [];
-                }
-            }
-
-            if (stateValue.History.length >= maxArrayLength) {
-                stateValue.History = [];
-            }
-
-            stateValue.History.push(Date.now());
-            var buttonCounter = (stateValue.History.length - 1).toString();
-
-            console.log(JSON.stringify(stateValue));
-            setState(statePathClickHistory, JSON.stringify(stateValue));
-
-            // Execute the button Action
-            var buttonAction = buttonActionsMap[buttonCounter];
-            if (buttonAction) {
-                buttonAction.buttonAction();
-            }
-        };
+        var buttonEventId = obj.state.val;
+        var buttonActionsArray = buttonActions[buttonEventId];
         
+        if (!buttonActionsArray){
+            return;
+        }
+        if (buttonActionsArray.length == 0) {
+            return
+        }
 
-        createState(statePathClickHistory, "{\"Event\":" + hueDimmerButtonId +",\"History\" : []}", () => {
-            createState(statePathLastButton, getButtonFromButtonId(hueDimmerButtonId), registerEvents);
+        // 1004 becomes 1000
+        var buttonId = Math.floor((buttonEventId / 1000)) * 1000; 
+
+        var rootStatePath = "Sensors.HueDimmer";
+        var statePathLastClickedButtonId = rootStatePath + "." + dimmerName + "." + "LastClickedButtonId";
+        var statePathDoubleClickHistory = rootStatePath + "." + dimmerName + "." + buttonEventId;
+
+
+        createState(statePathDoubleClickHistory, "{\"History\" : []}", () => {
+            createState(statePathLastClickedButtonId, buttonId, () =>  {
+
+                var lastClickedButtonId = getState(statePathLastClickedButtonId).val;
+                setState(statePathLastClickedButtonId, buttonId);
+
+                var doubleClickHistory = JSON.parse(getState(statePathDoubleClickHistory).val);
+                if (! (lastClickedButtonId == buttonId)) {
+                    // Forget history if different button was pressed earlier
+                    doubleClickHistory.History = [];
+                }
+                else if (doubleClickHistory.History.length >= 1) {
+                    var timeTheLastButtonWasPress =  doubleClickHistory.History[Math.max(doubleClickHistory.History.length - 1)];
+                    var currentTime = Date.now();
+                    if ((currentTime - timeTheLastButtonWasPress) > 3000)  {
+                        // Not a double press if too much time in-between clicks
+                        doubleClickHistory.History = [];
+                    }
+                }
+
+                if (doubleClickHistory.History.length >= buttonActionsArray.length) {
+                    // Reset cycle on too many presses
+                    doubleClickHistory.History = [];
+                }
+
+                doubleClickHistory.History.push(Date.now());
+                setState(statePathDoubleClickHistory, JSON.stringify(doubleClickHistory));
+
+                // Execute the button Action
+                var clickCounter = doubleClickHistory.History.length - 1;
+                var buttonAction = buttonActionsArray[clickCounter];
+                buttonAction.buttonAction();
+            });
         });
     });
 }
 
-registerHueDimmerButtonEvents("A1", 'deconz.0.Sensors.2.buttonevent', {
-    1000 : [
+registerHueDimmerButtonEvents("MyHueDimmerSwitch01", 'deconz.0.Sensors.2.buttonevent', {
+    
+    /* ON Button Down */ 1000  : [
            {
                 "buttonAction": () => {
-                    console.log(0);
+                    console.log("ON Button Down Singlepress");
                     setStateDelayed('hue.0.Hue_01.Wohnzimmer.bri', 254, false, parseInt("0", 10), false);
                 }
            },
            {
                 "buttonAction": () => {
-                    console.log(1);
+                    console.log("ON Button Down Doublepress");
                     setStateDelayed('hue.0.Hue_01.Wohnzimmer.bri', 200, false, parseInt("0", 10), false);
                 }
            },
            {
                 "buttonAction": () => {
-                    console.log(2);
+                    console.log("ON Button Down Triplepress");
                     setStateDelayed('hue.0.Hue_01.Wohnzimmer.bri', 175, false, parseInt("0", 10), false);
                 }
-           },
-           {
-                "buttonAction": () => {
-                    console.log(3);
-                    setStateDelayed('hue.0.Hue_01.Wohnzimmer.bri', 100, false, parseInt("0", 10), false);
-                }
-           },
-           {
-                "buttonAction": () => {
-                    console.log(4);
-                    setStateDelayed('hue.0.Hue_01.Wohnzimmer.bri', 50, false, parseInt("0", 10), false);
-                }
-           },
-    ],
-    2002 : [
-           {
-                "buttonAction": () => {
-                    console.log(0);
-                    setStateDelayed('hue.0.Hue_01.Wohnzimmer.bri', 100, false, parseInt("0", 10), false);
-                }
            }
     ],
-    3002 : [
-           {
+    /* ON Button Up */ 1002  : [],
+    /* ON Longpress */ 1003  : [],  
+    /* BRIGHTER Button Down */ 2000 : [],
+    /* BRIGHTER Button Up */ 2002 : [],
+    /* BRIGHTER Longpress */ 2003 : [],
+    /* DARKER Button Down*/ 3000 : [],
+    /* DARKER Button Up */ 3002 : [],
+    /* DARKER Longpress*/ 3003 : [],
+    /* OFF Button Down*/ 4000 : [
+            {
                 "buttonAction": () => {
-                    console.log(0);
-                    setStateDelayed('hue.0.Hue_01.Wohnzimmer.bri', 50, false, parseInt("0", 10), false);
-                }
-           }
-    ],
-    4002 : [
-           {
-                "buttonAction": () => {
-                    console.log(0);
+                    console.log("OFF Button Down Singlepress");
                     setStateDelayed('hue.0.Hue_01.Wohnzimmer.bri', 0, false, parseInt("0", 10), false);
                 }
-           }
-    ],
+            }],
+    /* OFF Button Up*/ 4002 : [],
+    /* OFF Longpress*/ 4003 : []
 });
 
